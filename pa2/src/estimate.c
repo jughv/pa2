20a12efb97ba9d2ed330d8ac54597cf9a34e0131
julio <50313766+jughv@users.noreply.github.com>
2021-10-28 06:21:16 -0400

    Add files via upload

--- pa2/src/estimate.c
+++ pa2/src/estimate.c
@@ -1,9 +1,8 @@
-
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <stdio.h>
-#define DEBUG 1
+#define DEBUG 0
 
 void freedom(double **matrix, int r){ //free columns of matrices
     for (int i =0;i<r;i++){
@@ -17,17 +16,14 @@ void print(double** matrix, int r){ //prints final answer
         
         printf("%.0f\n", matrix[i][0]);
     }
-
-
     return;
 }
-
 */
 
 void printmat(double **mat, int matrow, int matcol){
     for (int a = 0; a < matrow; a++){
         for (int b = 0; b < matcol; b++){
-            printf("%.1f", mat[a][b]);
+            printf("%.5f", mat[a][b]);
             if (b+1 == matcol){
                 continue;
             }
@@ -47,7 +43,7 @@ double **identity(int c){ //creates the identity matrix
     /*
     double **res = (double**)malloc(c*sizeof(double*));
     for (int i = 0; i < c; i++){
-        res[i] = (double*)malloc(c*sizeof(double));   //allocate columns
+        res[i] = -0(double*)malloc(c*sizeof(double));   //allocate columns
     }
     */
 
@@ -71,7 +67,7 @@ double **identity(int c){ //creates the identity matrix
 }
 
 
-double **inverse(double **M, double **I, int r, int c){ //must invert the matrixes properly
+double **inverse(double **M, double **I, int n){ //must invert the matrixes properly
 //   double **res = (double**)malloc(r*sizeof(double*));
 //   double **temp =M;
 //    for (int i = 0; i < r; i++){
@@ -81,55 +77,158 @@ double **inverse(double **M, double **I, int r, int c){ //must invert the matrix
     double **temp = M;
     double f,g; //pivoter
 
-    for (int i =0; i<r; i++){
+    /*
+    for (int i =0; i<n; i++){
         f = temp[i][i];
 
-        for(int p = 0; p<r;p++){
+        for(int p = 0; p<n;p++){
                 temp[i][p] = temp[i][p] / f;
                 I[i][p] = I[i][p] / f;
         }//end small for
 
-        for (int p = 0; p<c; p++){
+        for (int p = 0; p<n; p++){
             g = temp[p][i];
             if(p == i){} //skip identities
             else{
-                for (int x = 0; x<c; x++){
+                for (int x = 0; x<n; x++){
                     temp[p][x]= M[p][x]- (g * temp[i][x]);
                     I[p][x]= I[p][x] - (g * I[i][x]);
+                    if(DEBUG){
+                        printf("current matrix:\n");
+                        printmat(I, n, n);
+                        printf("\n");
+                    }
                 }
             }//end else
         }
 
+
+
     }//end big for
+    */
+
+    
+   for (int i =0; i<n; i++){
+        f = temp[i][i];
+
+        for(int p = 0; p<n;p++){
+                temp[i][p] = temp[i][p] / f;
+                I[i][p] = I[i][p] / f;
+        }//end small for
+        if(DEBUG){
+                        printf("it1:\n");
+                        printf("current matrix:\n");
+                        printmat(I, n, n);
+                        printf("\n");
+
+                        printf("old matrix:\n");
+                        printmat(temp, n, n);
+                        printf("\n");
+                    }
+
+        for (int p =i+1; p<n; p++){
+            g = temp[p][i];
+            
+            
+                for (int x = 0; x<n; x++){
+                    //if(p == i  ){continue;}
+                    temp[p][x]= temp[p][x]- (g * temp[i][x]);
+                    I[p][x]= I[p][x] - (g * I[i][x]);
+                    //temp[x][p]= temp[x][p]- (g * temp[i][p]);
+                    //I[x][p]= I[x][p] - (g * I[i][p]);
+                
+            }//end else
+        }
+        if(DEBUG){
+                        printf("it2:\n");
+                        printf("current matrix:\n");
+                        printmat(I, n, n);
+                        printf("\n");
+
+                        printf("old matrix:\n");
+                        printmat(temp, n, n);
+                        printf("\n");
+                    }
+
+    }
+
+
+    for(int i = n-1;i>=0;i--){
+        for (int p = i-1; p>=0; p--){
+            g = temp[p][i];
+            
+            
+                for (int x = n-1; x>=0; x--){
+                    if(p == i ){continue;}
+                    temp[p][x]= temp[p][x]- (g * temp[i][x]);
+                    I[p][x]= I[p][x] - (g * I[i][x]);
+                   //temp[x][p]= temp[x][p]- (g * temp[i][p]);
+                    //I[x][p]= I[x][p] - (g * I[i][p]);
+                
+            }//end else
+        }
+        if(DEBUG){
+                        printf("current matrix:\n");
+                        printmat(I, n, n);
+                        printf("\n");
+
+                        printf("old matrix:\n");
+                        printmat(temp, n, n);
+                        printf("\n");
+                    }
+
+    }
+    
+        if(DEBUG){
+                        printf("current matrix:\n");
+                        printmat(I, n, n);
+                        printf("\n");
+
+                        printf("old matrix:\n");
+                        printmat(temp, n, n);
+                        printf("\n");
+                    }
+
+
     return I;
  //   return res;
 }
+
+
+
 
 
 double** matmulti(double **mat1, double **mat2, int mat1col, int mat1row, int mat2col){ //multiplies matrixes
         /*
     double  **res = (double**) malloc (mat1row*sizeof(double));//allocate rows
     int temp = 0;
-
-
     for (int i = 0; i < mat1row; i++){
         res[i] = (double*)malloc(mat2col*sizeof(double));   //allocate columns
     }   
     */
-   double temp = 0.0;
+   //double temp = 0.0;
+
    double **res = malloc(mat1row*sizeof(double*));
     for (int i = 0; i < mat1row; i++){
         res[i] = malloc(mat2col*sizeof(res[0]));   //allocate columns
     }
+    
 
 
     for (int i = 0; i < mat1row; i++){ //multiply
-        for(int k = 0; k < mat2col; k++){
-            for (int j = 0; j < mat1col; j++){
-                temp = (mat1[i][j] * mat2[j][k]) + temp;
+        for(int j = 0; j < mat2col; j++){
+            res[i][j] =0;
+            for (int k = 0; k < mat1col; k++){
+                //temp = (mat1[i][j] * mat2[j][k]) + temp;
+                res[i][j] += mat1[i][k] *mat2[k][j];
+                /*if(DEBUG){
+                    printf("%lf\n", res[i][j]);
+                }*/
             }
-            res[i][k] = temp;
-            temp = 0.0;
+            //res[i][j] = temp;
+           // temp = 0.0;
+            
+            
             
         }
     }
@@ -292,7 +391,9 @@ int main (int argc, char **argv){
     printf("\n");
     }
 
-    double **testinverse= inverse(current, I, traincolumns, traincolumns); //inverse the multiple  (X^TX)^-1
+    //double **testinverse= inverse(current, I, traincolumns, traincolumns); //inverse the multiple  (X^TX)^-1
+    double **testinverse= inverse(current,I, traincolumns); //inverse the multiple  (X^TX)^-1
+
 
     if(DEBUG){
     printf("testinverse:\n");
@@ -343,7 +444,4 @@ freedom(answer,datarows);
 
     return 0;
 }
-
-
-
 
